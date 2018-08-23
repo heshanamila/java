5e3cb9f6396937f950a842db338d3fcdeb3d641c
Heshan_Amila_Chandrasena <32818096+heshanamila@users.noreply.github.com>
2018-08-23 12:45:35 +0530

    Add files via upload

---
+++ starpattern.java
@@ -0,0 +1,45 @@
+ class starpattern{
+    public static void main (String args[]){
+		
+        for(int i=0;i<10;i++){
+			for(int j=0;j<10;j++){
+				System.out.print("*");
+			}
+        
+         System.out.println();
+        }
+		
+		System.out.println("---------------------");
+		
+		
+		for(int i=0;i<10;i++){
+			for(int j=10-i;j>0;j--){
+				System.out.print("*");
+				
+			}
+		 System.out.println();
+		
+		}
+		
+		System.out.println("----------------------");
+		
+		for(int i=0;i<10;i++){
+			for(int j=10-i;j>0;j--){
+				System.out.print(" ");
+			}
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+				
+		 System.out.println();
+		 
+		
+		} 
+	    
+		System.out.println("----------------------");
+	
+    
+        
+    }
+    
+}
