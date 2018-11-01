b06bd9cc40137ed331bbd366d0abefd042bbac99
Heshan_Amila_Chandrasena <32818096+heshanamila@users.noreply.github.com>
2018-11-01 23:36:46 +0530

    Add files via upload

---
+++ new1.java
@@ -0,0 +1,12 @@
+class new1{
+	public static void main(String args[]){
+	 int myarraynum[] = new int[5];
+	 int i =0;
+	 for(i=0;i<myarraynum.length;i++){
+		myarraynum[i] = i;
+		System.out.println(i);
+	    }
+	
+	
+	}
+}
